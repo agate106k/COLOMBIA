e543988275c6e17dbf988dd87a2859083196315f
RioTsukiji <87016067+RioTsukiji@users.noreply.github.com>
2021-12-29 01:22:28 +0900

    first commit

--- components/IconsFlex.tsx
+++ components/IconsFlex.tsx
@@ -3,20 +3,22 @@ import {View} from "../components/Themed";
 import {Avatar, ListItem, colors, Button} from "react-native-elements";
 import {messages_list} from "./messages";
 import React from "react";
-
+import 'react-native-gesture-handler';
+import ChatScreen from "../screens/ChatScreen";
 
 
 export default function IconsFlex () {
     return (
         <View style={styles.list}>
             {messages_list.map((user)=>(
-                <ListItem key={user.id} bottomDivider>
+                <ListItem key={user.id} bottomDivider onPress={() => this.props.navigation.navigate('ChatScreen')}>
                     <Avatar rounded source={user.pic} />
                         <ListItem.Title style={styles.title}>{user.title}</ListItem.Title>
                         <ListItem.Subtitle style={styles.subtitle}>{user.message}</ListItem.Subtitle>
                     {/*文字数が増えてもレイアウトがずれないようにする*/}
                     <ListItem.Chevron />
                 </ListItem>
+
             ))
             }
         </View>
